90a00ffe044cd647b699082adccd3b21108bfef2
RoaaFrehat1 <r.frehat1@student.aaup.edu>
2024-06-06 21:58:02 +0300

    first

--- main.js
+++ main.js
@@ -1,2 +1,20 @@
-document.querySelector(".userprofile h2").textContent="Hello Roaa"
-document.querySelector(".userinfo p").classList.add("Roaa");
+var change=document.querySelector(".fontSize");
+
+ 
+ var largF=document.querySelector(".larg");
+
+
+var smallF=document.querySelector(".smallBtn");
+console.log(change);
+console.log(largF);
+console.log(smallF);
+function changeFontL(){
+    change.classList.add("larg");
+    change.classList.remove("smallF");
+}
+function changeFontS(){
+    change.classList.add("smallF");
+    change.classList.remove("larg");
+}
+largF.onclick=changeFontL;
+smallF.onclick=changeFontS;
