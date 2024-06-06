f0e8ba6aa7288e3f9fba176fc49794e26cb84983
RoaaFrehat1 <r.frehat1@student.aaup.edu>
2024-06-06 22:07:34 +0300

    first

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var change=document.querySelector(".fontSize");
 
  
- var largF=document.querySelector(".larg");
+ var largF=document.querySelector(".large");
 
 
 var smallF=document.querySelector(".smallBtn");
@@ -9,12 +9,12 @@ console.log(change);
 console.log(largF);
 console.log(smallF);
 function changeFontL(){
-    change.classList.add("larg");
+    change.classList.add("large");
     change.classList.remove("smallF");
 }
 function changeFontS(){
     change.classList.add("smallF");
-    change.classList.remove("larg");
+    change.classList.remove("large");
 }
 largF.onclick=changeFontL;
 smallF.onclick=changeFontS;
